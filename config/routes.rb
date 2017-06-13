d14440cd07f44c3c7fb1fa0e110c60f281d5994f
Cindy <lxx0314@126.com>
2017-06-13 14:31:45 +0800

    add post button

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :groups do
     resources :posts
   end
-  root 'groups#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
 end
